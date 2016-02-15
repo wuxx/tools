2e434f9b1d34bfbe3af4be3f0d047a51df9adf34
wuxx <wujianhua0924@gmail.com>
2016-02-15 15:04:26 +0000

    update

--- getip.sh
+++ getip.sh
@@ -2,4 +2,5 @@
 
 WLAN_IP=$(curl -s -u admin:admin http://192.168.1.1/userRpm/StatusRpm.htm | grep -A1 "6C-E8-73-6F-A6-D5" | tail -n 1 | sed 's/,//')
 
-echo $WLAN_IP
+DATE=$(date)
+echo "${DATE} $WLAN_IP" > README.md
