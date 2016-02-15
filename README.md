fe56b071e99e4795a69a832164eb9a1dcdba3860
wuxx <wujianhua0924@gmail.com>
2016-02-15 15:06:50 +0000

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 some useful tools & script collected.
+Mon Feb 15 15:06:25 UTC 2016 "49.221.185.77"
+Mon Feb 15 15:06:50 UTC 2016 "49.221.185.77"
--- getip.sh
+++ getip.sh
@@ -3,4 +3,7 @@
 WLAN_IP=$(curl -s -u admin:admin http://192.168.1.1/userRpm/StatusRpm.htm | grep -A1 "6C-E8-73-6F-A6-D5" | tail -n 1 | sed 's/,//')
 
 DATE=$(date)
-echo "${DATE} $WLAN_IP" > README.md
+echo "${DATE} $WLAN_IP" >> README.md
+git add -A
+git commit -m "update"
+git push origin master
