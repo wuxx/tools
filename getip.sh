8d2cef343b3ea25e19dbe5a540dd0720e03365cc
root <root@raspberrypi.(none)>
2016-02-15 23:15:01 +0000

    update

--- getip.sh
+++ getip.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+DIR=/home/pi/tools
 WLAN_IP=$(curl -s -u admin:admin http://192.168.1.1/userRpm/StatusRpm.htm | grep -A1 "6C-E8-73-6F-A6-D5" | tail -n 1 | sed 's/,//')
 
 DATE=$(date)
-echo "${DATE} $WLAN_IP" >> README.md
+echo "${DATE} $WLAN_IP" >> ${DIR}/README.md
+cd ${DIR}
 git add -A
 git commit -m "update"
 git push origin master
