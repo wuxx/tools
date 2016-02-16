6e620a975546b1c9cf21bb6afae5a3c06eba9f0c
wuxx <wujianhua0924@gmail.com>
2016-02-16 21:16:41 +0000

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 some useful tools & script collected.
 Mon Feb 15 23:21:47 UTC 2016 "49.221.185.77"
 Mon Feb 15 23:21:47 UTC 2016 "49.221.185.77"
-Mon Feb 15 23:21:47 UTC 2016 "49.221.185.77"
+Tue Feb 16 21:16:41 UTC 2016 "49.221.185.77"
--- getip.sh
+++ getip.sh
@@ -3,9 +3,13 @@
 DIR=/home/pi/tools
 WLAN_IP=$(curl -s -u admin:admin http://192.168.1.1/userRpm/StatusRpm.htm | grep -A1 "6C-E8-73-6F-A6-D5" | tail -n 1 | sed 's/,//')
 
-DATE=$(date)
-echo "${DATE} $WLAN_IP" >> ${DIR}/README.md
-cd ${DIR}
-git add -A
-git commit -m "update"
-git push origin master
+echo ${WLAN_IP}
+
+if [ "$1" = "--commit" ]; then
+    DATE=$(date)
+    echo "${DATE} $WLAN_IP" >> ${DIR}/README.md
+    cd ${DIR}
+    git add -A
+    git commit -m "update"
+    git push origin master
+fi
