bd71cb13af2c560e3d6b33fcfe89551b870221a9
wuxx <wujianhua0924@gmail.com>
2016-04-02 23:36:53 +0000

    update.

--- vim_init.sh
+++ vim_init.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-echo 'set nu' >> ~/.vimrc
-echo 'set hlsearch' >> ~/.vimrc
-echo 'syntax on' >> ~/.vimrc
+echo 'set nu'        >> ~/.vimrc
+echo 'set hlsearch'  >> ~/.vimrc
+echo 'syntax on'     >> ~/.vimrc
+echo 'set ts=4'      >> ~/.vimrc
+echo 'set expandtab' >> ~/.vimrc
 
 #autocmd BufReadPost *  if line("'\"")>0&&line("'\"")<=line("$") | exe "normal g'\"" |  endif
