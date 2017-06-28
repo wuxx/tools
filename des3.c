98ad2b77e302ddfaf0212ab0039ab17625c3f50d
wuxx <wujianhua0924@gmail.com>
2017-06-28 22:02:43 +0800

    1. sensor3in1 ok

--- des3.c
+++ des3.c
@@ -3,24 +3,23 @@
 #include <openssl/evp.h>
 #include <openssl/x509.h>
  
-
+/* gcc des3.c -lssl -lcrypto */
 void tEVP_Encrypt()
 {
-    unsigned char key[EVP_MAX_KEY_LENGTH];  //密钥
-    unsigned char iv[EVP_MAX_KEY_LENGTH];//初始化向量
-    EVP_CIPHER_CTX ctx;//EVP算法上下文
-    unsigned char out[1024];//输出密文缓冲区
-    unsigned char out1[1024];//输出明文缓冲区
-    int outl;//密文长度
+    unsigned char key[EVP_MAX_KEY_LENGTH]; 
+    unsigned char iv[EVP_MAX_KEY_LENGTH];
+    EVP_CIPHER_CTX ctx;
+    unsigned char out[1024];
+    unsigned char out1[1024];
+    int outl;
     int outltmp;
-    int outl1;//明文长度
+    int outl1;
     int outltmp1;
-    char *msg="Hello OpenSSL!";//待加密的数据
+    char *msg="Hello OpenSSL!";
     int rv;
     int rv1;
     int i;
-    //加密过程
-    //设置key和iv（可以采用随机数和可以是用户输入）
+
     for(i=0;i<24;i++)
     {
         key[i]=i;
@@ -29,23 +28,23 @@ void tEVP_Encrypt()
     {
         iv[i]=i;
     }
-    //初始化密码算法结构体
+
     EVP_CIPHER_CTX_init(&ctx);
-    //设置算法和密钥
+
     rv = EVP_EncryptInit_ex(&ctx,EVP_des_ede3_cbc(),NULL,key,iv);
     if(rv!=1)
     {
         printf("Err\n");
         return;
     }
-    //数据加密
+
     rv = EVP_EncryptUpdate(&ctx,out,&outl,msg,strlen(msg));
     if(rv!=1)
     {
         printf("Err\n");
         return;
     }
-    //结束数据加密，把剩余数据输出。
+
     rv = EVP_EncryptFinal_ex(&ctx,out+outl,&outltmp);
     if(rv!=1)
     {
@@ -53,9 +52,9 @@ void tEVP_Encrypt()
         return;
     }
     outl = outl +outltmp;   
-    printf("原文为:%s\n",msg);
-    //打印输出密文
-    printf("密文长度：%d\n密文数据：\n",outl);
+    printf("%s \n",msg);
+
+    printf("%d \n",outl);
     for(i=0;i<outl;i++)
     {
         printf("0x%02x ",out[i]);
---
+++ python/1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import time
+import smbus
+
+print ("123")
+bus = smbus.SMBus(1)
+print (bus)
+
