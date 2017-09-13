7a85a2ded10201e22a0f3d5a18443e211f815899
wuxx <wujianhua0924@gmail.com>
2017-09-13 16:35:59 +0800

    update  Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   gpio_test.c

--- rpi/gpio_test.c
+++ rpi/gpio_test.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
 #include <wiringPi.h>
 
 /* pi@raspberrypi:~$ gcc gpio_test.c -lwiringPi */
@@ -14,6 +18,7 @@
 
 */
 
+#if 0
 int main(void)
 {
   wiringPiSetup() ;
@@ -24,4 +29,48 @@ int main(void)
     digitalWrite(8,  LOW) ; delay (500) ;
   }
 }
+#endif
 
+// What GPIO input are we using?
+//      This is a wiringPi pin number
+
+#define BUTTON_PIN      (8)
+
+static volatile int globalCounter = 0 ;
+
+void myInterrupt (void)
+{
+  ++globalCounter ;
+}
+
+
+int main (void)
+{
+  int myCounter = 0 ;
+
+  if (wiringPiSetup () < 0)
+  {
+    fprintf (stderr, "Unable to setup wiringPi: %s\n", strerror (errno)) ;
+    return 1 ;
+  }
+
+  if (wiringPiISR (BUTTON_PIN, INT_EDGE_FALLING, &myInterrupt) < 0)
+  {
+    fprintf (stderr, "Unable to setup ISR: %s\n", strerror (errno)) ;
+    return 1 ;
+  }
+
+
+  for (;;)
+  {
+    printf ("Waiting ... ") ; fflush (stdout) ;
+
+    while (myCounter == globalCounter)
+      delay (100) ;
+
+    printf (" Done. counter: %5d\n", globalCounter) ;
+    myCounter = globalCounter ;
+  }
+
+  return 0 ;
+}
