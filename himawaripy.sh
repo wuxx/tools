0d3dca41dd7100ba7d208091c6d1958679faae33
musex <447830120@qq.com>
2018-06-23 17:32:33 +0800

    update

--- himawaripy.sh
+++ himawaripy.sh
@@ -7,6 +7,9 @@ while [ 1 ]; do
     date
     #rm /homm/pi/.cache/himawaripy/*.png
 
+    YESTERDAY=`date -d '1 days ago' +%F`
+    find /home/pi/.cache/himawaripy/ -type f -newermt "${YESTERDAY} 00:00:00" ! -newermt "${YESTERDAY} 23:59:59" | xargs rm
+    #find /home/pi/.cache/himawaripy/ -type f -newermt "2018-06-14 00:00:00" ! -newermt "2018-06-14 23:59:59"
     #find /home/pi/.cache/himawaripy/ -type f -newermt `date -d '1 days ago' +%F` | xargs rm
 
     timeout 300 /usr/local/bin/himawaripy
