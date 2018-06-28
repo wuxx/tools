beab22a574588be733dab61c762ed64eea2a600e
musex <447830120@qq.com>
2018-06-28 20:15:34 +0800

    update

--- jtag/stm32/attach.sh
+++ jtag/stm32/attach.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-sudo openocd -f  tcl/target/stm32f1x_sysfsgpio.cfg
+#sudo openocd -f tcl/target/stm32f1x_sysfsgpio.cfg -c 'init; halt; resume; '
+
+sudo openocd -f tcl/target/stm32f1x_sysfsgpio.cfg -c \
+"   init;
+    halt;
+    flash write_image erase /home/pi/oss/tools/jtag/stm32/LED_300.hex;
+    reset;
+    shutdown;
+"
+
+
