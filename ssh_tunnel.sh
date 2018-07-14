0908394d7a454967a81745346c358869714bacce
musex <447830120@qq.com>
2018-07-14 16:45:54 +0800

    update

--- ssh_tunnel.sh
+++ ssh_tunnel.sh
@@ -8,7 +8,8 @@
 while [ 1 ]; do
 
     echo "check ssh tunnel"
-    ps aux | grep TCPKeepAlive | grep -v grep
+    #ps aux | grep TCPKeepAlive | grep -v grep
+    ps aux | grep StrictHostKeyChecking | grep -v grep
     if [ $? -ne 0 ]; then
         echo "start ssh tunnel"
         #ssh -N -f -o TCPKeepAlive=yes -R 2222:127.0.0.1:22  root@123.56.12.242
