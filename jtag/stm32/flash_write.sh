2d541a2e455fefb813a58912712c6e21bca99a6d
deployer2018 <deployer2018@qq.com>
2018-08-17 11:00:59 +0800

    update

--- jtag/stm32/flash_write.sh
+++ jtag/stm32/flash_write.sh
@@ -12,14 +12,26 @@ IMAGE_FILE=$1
 FLASH_ADDR=0x08000000
 FLASH_SIZE=0x00020000
 
-#flash write_image erase xxx.hex 0x08000000;
-#flash write_image erase xxx.bin;
+EXT="${IMAGE_FILE##*.}"
+echo EXT: $EXT
+
+if [ "${EXT}" == "hex" ]; then
+    TARGET="$IMAGE_FILE"
+elif [ "${EXT}" == "bin" ]; then
+    TARGET="$IMAGE_FILE $FLASH_ADDR"
+else
+    echo "illegal suffix [$EXT]"
+    exit 1
+fi
+
+#flash write_image erase xxx.hex;
+#flash write_image erase xxx.bin 0x08000000;
 
 sudo openocd -f ${CURRENT_DIR}/tcl/target/stm32f1x_sysfsgpio.cfg -c \
 "   init;
     halt;
     reset halt;
-    flash write_image erase $IMAGE_FILE 0x08000000;
+    flash write_image erase ${TARGET};
     reset;
     shutdown;
 "
