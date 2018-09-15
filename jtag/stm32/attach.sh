fb9e95466326f023739a15f70a800af826a887e5
deployer2018 <deployer2018@qq.com>
2018-09-15 13:07:31 +0800

    update

--- jtag/stm32/attach.sh
+++ jtag/stm32/attach.sh
@@ -2,4 +2,6 @@
 
 #sudo openocd -f tcl/target/stm32f1x_sysfsgpio_swd.cfg
 
-sudo openocd -f tcl/target/stm32f1x_bcm2835gpio_swd.cfg
+#sudo openocd -f tcl/target/stm32f1x_bcm2835gpio_swd.cfg
+
+sudo openocd -f tcl/target/stm32f2x_sysfsgpio_swd.cfg
