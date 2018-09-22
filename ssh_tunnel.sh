fb15fd5f3d78f4293ead8424d040c1b7a66a4012
deployer2018 <deployer2018@qq.com>
2018-09-22 10:25:15 +0800

    update

--- ssh_tunnel.sh
+++ ssh_tunnel.sh
@@ -9,11 +9,12 @@ while [ 1 ]; do
 
     echo "check ssh tunnel"
     #ps aux | grep TCPKeepAlive | grep -v grep
-    ps aux | grep StrictHostKeyChecking | grep -v grep
+    ps aux | grep autossh | grep -v grep
     if [ $? -ne 0 ]; then
         echo "start ssh tunnel"
         #ssh -N -f -o TCPKeepAlive=yes -R 2222:127.0.0.1:22  root@123.56.12.242
-        ssh -N -f -o "StrictHostKeyChecking no" -R 2222:127.0.0.1:22  tester@123.56.12.242
+        #ssh -N -f -o "StrictHostKeyChecking no" -R 2222:127.0.0.1:22  tester@123.56.12.242
+        autossh -M 2221 -NR 2222:localhost:22 tester@123.56.12.242 -p 22
     else
         echo "ssh tunnel already open, sleep... "
         sleep 60
