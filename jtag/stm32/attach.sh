5c170bae77103214f63a83a2fa8196b8d15e6a7e
deployer2018 <deployer2018@qq.com>
2018-09-28 13:47:47 +0800

    update

--- jtag/stm32/attach.sh
+++ jtag/stm32/attach.sh
@@ -2,12 +2,12 @@
 
 #sudo openocd -f tcl/target/stm32f1x_sysfsgpio_swd.cfg
 
-#sudo openocd -f tcl/target/stm32f1x_bcm2835gpio_swd.cfg
 
 
 #CONFIG=${CURRENT_DIR}/tcl/target/stm32f1x_cmsisdap.cfg
 
 #sudo openocd -f tcl/target/stm32f2x_sysfsgpio_swd.cfg
 
-sudo openocd -f tcl/target/stm32f1x_cmsisdap.cfg
+#sudo openocd -f tcl/target/stm32f1x_cmsisdap.cfg
 
+sudo openocd -f tcl/target/stm32f1x_bcm2835gpio_swd.cfg
