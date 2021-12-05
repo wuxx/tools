07d89fe066a2c7f3de452e937726af55c6659906
wuxx <wujianhua0924@gmail.com>
2021-12-05 13:50:24 +0800

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   jtag/stm32/flash_dump.sh
    	modified:   jtag/stm32/wait_and_probe.sh
    	modified:   jtag/stm32/wait_detach.sh

--- jtag/stm32/wait_and_probe.sh
+++ jtag/stm32/wait_and_probe.sh
@@ -10,18 +10,12 @@ while [ 1 ]; do
 
     DAP_INFO=$(lsusb | grep "0d28:0204")
 
-    if [ ${#DAP_INFO} -ne 0 ]; then
-        echo -n "downloader: $DAP_INFO "
-    else
-        echo -n "x"
-    fi
-
     /home/pi/oss/tools/jtag/stm32/probe.sh
     if [ $? -eq 0 ]; then
-        echo "target connected"
+        echo "target connected [$DAP_INFO]"
         exit 0
     else
-        echo "."
+        echo "wait attach <<< [$DAP_INFO]"
     fi
 
 done
--- jtag/stm32/wait_detach.sh
+++ jtag/stm32/wait_detach.sh
@@ -8,10 +8,11 @@ function onCtrlC () {
 
 while [ 1 ]; do
 
+    DAP_INFO=$(lsusb | grep "0d28:0204")
 
     /home/pi/oss/tools/jtag/stm32/probe.sh
     if [ $? -eq 0 ]; then
-        echo "wait detach"
+        echo "wait detach >>> [$DAP_INFO]"
     else
         exit 0
     fi
