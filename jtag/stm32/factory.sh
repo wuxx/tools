5c661425bd30883d96ab74927fd7ab998fd6033c
wuxx <wujianhua0924@gmail.com>
2022-03-07 11:34:12 +0800

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   ../../impedance_match
    	new file:   buzzer_fail
    	new file:   buzzer_succ
    	modified:   factory.sh
    	modified:   flash_stm32_lock.sh

--- jtag/stm32/factory.sh
+++ jtag/stm32/factory.sh
@@ -18,6 +18,27 @@ function onCtrlC () {
     exit 0
 }
 
+BUZZER_GPIO=4
+
+gpio mode $BUZZER_GPIO out
+
+function buzzer_succ () {
+    gpio write $BUZZER_GPIO 1
+    sleep 0.1
+    gpio write $BUZZER_GPIO 0
+    sleep 0.1
+}
+
+function buzzer_fail () {
+    for((i=0;i<3;i++)); do
+        gpio write $BUZZER_GPIO 1
+        sleep 0.1
+        gpio write $BUZZER_GPIO 0
+        sleep 0.1
+    done
+
+}
+
 while [ 1 ]; do
     ${CURRENT_DIR}/probe.sh
     if [ $? -eq 0 ]; then
@@ -27,10 +48,12 @@ while [ 1 ]; do
         if [ $? -eq 0 ]; then
             succ_count=$(($succ_count+1))  
             echo -e "\033[32m---------- SUCC [$succ_count] ----------\033[0m"
+            buzzer_succ
 
         else
             fail_count=$(($fail_count+1))  
             echo -e "\033[31m---------- FAIL [$fail_count] ----------\033[0m"
+            buzzer_fail
 
         fi
 
--- jtag/stm32/flash_stm32_lock.sh
+++ jtag/stm32/flash_stm32_lock.sh
@@ -20,12 +20,48 @@ $CURRENT_DIR/wait_and_probe.sh
 #exit 1
 #fi
 
+#BUZZER_GPIO=4 #wiringPi
+BUZZER_GPIO=23 #BCM
+
+#gpio mode $BUZZER_GPIO out
+raspi-gpio set $BUZZER_GPIO op
+
+function buzzer_succ () {
+    #gpio write $BUZZER_GPIO 1
+    raspi-gpio set $BUZZER_GPIO dh
+    sleep 0.1
+    raspi-gpio set $BUZZER_GPIO dl
+    sleep 0.1
+}
+
+function buzzer_fail () {
+    for((i=0;i<3;i++)); do
+        raspi-gpio set $BUZZER_GPIO dh
+        sleep 0.1
+        raspi-gpio set $BUZZER_GPIO dl
+        sleep 0.1
+    done
+
+}
+
+
 /home/pi/oss/tools/jtag/stm32/flash_write.sh $1
 if [ $? -ne 0 ]; then
-    echo "write fail!"
+    echo -e "\033[31m---------- write fail! ----------\033[0m"
+    buzzer_fail
     exit 1
+else
+    echo -e "\033[32m---------- write succ! ----------\033[0m"
 fi
 
 /home/pi/oss/tools/jtag/stm32/lock.sh
+if [ $? -ne 0 ]; then
+    echo -e "\033[31m---------- lock fail! ----------\033[0m"
+    buzzer_fail
+    exit 1
+else
+    echo -e "\033[32m---------- lock succ! ----------\033[0m"
+    buzzer_succ
+fi
 
 $CURRENT_DIR/wait_detach.sh
